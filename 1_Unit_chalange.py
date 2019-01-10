4f180858d0e1604554b893b190276eb55f511c66
Richard <ricardas.dovgialo@bazaarvoice.com>
2019-01-10 11:26:50 +0200

    Create 1_Unit_chalange.py

---
+++ 1_Unit_chalange.py
@@ -0,0 +1,31 @@
+
+
+def adding_report(all_or_total):
+	total = 0
+	items = 'Items \n'
+	while True:
+		ux = input('Input an integer to add to the total or "Q" to quit ')
+		if ux.isnumeric():
+			ux = int(ux)
+			total = total + ux
+			if all_or_total.lower() == 'a':
+				ux = str(ux)
+				items = items + ux + '\n'
+			else:
+				pass
+		elif ux.lower().startswith('q'):
+			if all_or_total.lower() == 'a':
+				print(items)
+				total = str(total)
+				print('\nTotal\n' + total + '\n')
+				break
+			else:
+				total = str(total)
+				print('\nTotal\n' + total + '\n')
+				break
+		else:
+			print("Enter an integer or \"Q\":")
+
+
+
+
